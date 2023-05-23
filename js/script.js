38e5f1a055cd1591fdd0bcf1bc9f47b265d2bb05
LucasM548 <lucasm54800@gmail.com>
2023-05-24 01:45:21 +0200

    formatter

--- js/script.js
+++ js/script.js
@@ -20,60 +20,60 @@ const appLinks = [
 const appPanel = document.getElementById("appPanel");
 
 appLinks.forEach((app) => {
-  const appElement = document.createElement("a");
-  appElement.href = app.href;
-  appElement.target = "_blank";
-  appElement.classList.add("app");
+    const appElement = document.createElement("a");
+    appElement.href = app.href;
+    appElement.target = "_blank";
+    appElement.classList.add("app");
 
-  const imgElement = document.createElement("img");
-  imgElement.src = app.imgSrc;
-  imgElement.alt = app.alt;
+    const imgElement = document.createElement("img");
+    imgElement.src = app.imgSrc;
+    imgElement.alt = app.alt;
 
-  const textElement = document.createElement("p");
-  textElement.textContent = app.text;
+    const textElement = document.createElement("p");
+    textElement.textContent = app.text;
 
-  appElement.appendChild(imgElement);
-  appElement.appendChild(textElement);
-  appPanel.appendChild(appElement);
+    appElement.appendChild(imgElement);
+    appElement.appendChild(textElement);
+    appPanel.appendChild(appElement);
 });
 
 function toggleAppPanel(event) {
-  event.stopPropagation();
-  const applicationsLabel = document.querySelector('label[for="Applications"]');
-  appPanel.classList.toggle("visible");
-  applicationsLabel.classList.toggle("active");
-  applicationsLabel.classList.toggle("hovered");
+    event.stopPropagation();
+    const applicationsLabel = document.querySelector('label[for="Applications"]');
+    appPanel.classList.toggle("visible");
+    applicationsLabel.classList.toggle("active");
+    applicationsLabel.classList.toggle("hovered");
 }
 
 function hideAppPanel() {
-  const applicationsLabel = document.querySelector('label[for="Applications"]');
-  appPanel.classList.remove("visible");
-  applicationsLabel.classList.remove("active");
-  applicationsLabel.classList.remove("hovered");
+    const applicationsLabel = document.querySelector('label[for="Applications"]');
+    appPanel.classList.remove("visible");
+    applicationsLabel.classList.remove("active");
+    applicationsLabel.classList.remove("hovered");
 }
 
-document.addEventListener("click", function (event) {
-  const applicationsLabel = document.querySelector('label[for="Applications"]');
-  if (!appPanel.contains(event.target) && event.target.id !== "Applications" && event.target.tagName !== "LABEL") {
-    hideAppPanel();
-  }
+document.addEventListener("click", function(event) {
+    const applicationsLabel = document.querySelector('label[for="Applications"]');
+    if (!appPanel.contains(event.target) && event.target.id !== "Applications" && event.target.tagName !== "LABEL") {
+        hideAppPanel();
+    }
 });
 
 const applicationsLabel = document.querySelector('label[for="Applications"]');
-applicationsLabel.addEventListener("mouseenter", function (event) {
-  if (appPanel.classList.contains("visible")) {
-    applicationsLabel.classList.add("hovered");
-  }
+applicationsLabel.addEventListener("mouseenter", function(event) {
+    if (appPanel.classList.contains("visible")) {
+        applicationsLabel.classList.add("hovered");
+    }
 });
 
-applicationsLabel.addEventListener("mouseleave", function (event) {
-  applicationsLabel.classList.remove("hovered");
+applicationsLabel.addEventListener("mouseleave", function(event) {
+    applicationsLabel.classList.remove("hovered");
 });
 
-document.addEventListener("click", function (event) {
-  if (!appPanel.contains(event.target) && event.target.id !== "Applications" && event.target.tagName !== "LABEL") {
-    hideAppPanel();
-  }
+document.addEventListener("click", function(event) {
+    if (!appPanel.contains(event.target) && event.target.id !== "Applications" && event.target.tagName !== "LABEL") {
+        hideAppPanel();
+    }
 });
 /*============================ END OF APPS PANEL ============================*/
 
@@ -82,40 +82,40 @@ document.addEventListener("click", function (event) {
  *               CHOIX MOTEUR
  *=============================================**/
 function getEngineURL(engine) {
-  switch (engine) {
-    case 'Google':
-      return 'https://www.google.com/search?q=';
-    case 'Brave_search':
-      return 'https://search.brave.com/search?q=';
-    case 'DuckDuckGo':
-      return 'https://duckduckgo.com/?q=';
-    default:
-      return '';
-  }
+    switch (engine) {
+        case 'Google':
+            return 'https://www.google.com/search?q=';
+        case 'Brave_search':
+            return 'https://search.brave.com/search?q=';
+        case 'DuckDuckGo':
+            return 'https://duckduckgo.com/?q=';
+        default:
+            return '';
+    }
 }
 
 function getSelectedEngine() {
-  const storedEngine = localStorage.getItem('selectedEngine');
-  if (storedEngine) {
-    return storedEngine;
-  } else {
-    return 'Brave_search';
-  }
+    const storedEngine = localStorage.getItem('selectedEngine');
+    if (storedEngine) {
+        return storedEngine;
+    } else {
+        return 'Brave_search';
+    }
 }
 
 function setSelectedEngine(engine) {
-  localStorage.setItem('selectedEngine', engine);
-  $('input[name="search_engine"][value="' + engine + '"]').prop('checked', true);
+    localStorage.setItem('selectedEngine', engine);
+    $('input[name="search_engine"][value="' + engine + '"]').prop('checked', true);
 }
 
 $(document).ready(function() {
-  const selectedEngine = getSelectedEngine();
-  setSelectedEngine(selectedEngine);
+    const selectedEngine = getSelectedEngine();
+    setSelectedEngine(selectedEngine);
 });
 
 $('input[name="search_engine"]').on('change', function() {
-  const newSelectedEngine = $('input[name="search_engine"]:checked').val();
-  setSelectedEngine(newSelectedEngine);
+    const newSelectedEngine = $('input[name="search_engine"]:checked').val();
+    setSelectedEngine(newSelectedEngine);
 });
 /*=============== FIN DU CHOIX MOTEUR ==============*/
 
@@ -124,33 +124,33 @@ $('input[name="search_engine"]').on('change', function() {
  *                           BOUTON RECHERCHE
  *========================================================================**/
 function searchToggle(obj, evt) {
-  const container = $(obj).closest('.search-wrapper');
-  const selectedEngine = getSelectedEngine();
+    const container = $(obj).closest('.search-wrapper');
+    const selectedEngine = getSelectedEngine();
 
-  if (!container.hasClass('active')) {
-    container.addClass('active');
-    evt.preventDefault();
-  } else if (
-    container.find('.search-input').val().trim() !== '' &&
-    (evt.target.classList.contains('search-icon') ||
-      evt.target.classList.contains('span') ||
-      (evt.type === 'keypress' && (evt.key === 'Enter' || evt.key === 'Return')))
-  ) {
-    const searchText = container.find('.search-input').val();
-    const webUrl = getEngineURL(selectedEngine);
-    if (webUrl) {
-      const finalURL = webUrl + encodeURIComponent(searchText);
-      window.location.href = finalURL;
+    if (!container.hasClass('active')) {
+        container.addClass('active');
+        evt.preventDefault();
+    } else if (
+        container.find('.search-input').val().trim() !== '' &&
+        (evt.target.classList.contains('search-icon') ||
+            evt.target.classList.contains('span') ||
+            (evt.type === 'keypress' && (evt.key === 'Enter' || evt.key === 'Return')))
+    ) {
+        const searchText = container.find('.search-input').val();
+        const webUrl = getEngineURL(selectedEngine);
+        if (webUrl) {
+            const finalURL = webUrl + encodeURIComponent(searchText);
+            window.location.href = finalURL;
+        }
+    } else if (container.hasClass('active') && !$(obj).closest('.input-holder').length) {
+        container.removeClass('active');
+        container.find('.search-input').val('');
     }
-  } else if (container.hasClass('active') && !$(obj).closest('.input-holder').length) {
-    container.removeClass('active');
-    container.find('.search-input').val('');
-  }
 }
 
-$('.search-input').on('keypress', function (evt) {
-  if (evt.key === 'Enter' || evt.key === 'Return') {
-    searchToggle(this, evt);
-  }
+$('.search-input').on('keypress', function(evt) {
+    if (evt.key === 'Enter' || evt.key === 'Return') {
+        searchToggle(this, evt);
+    }
 });
 /*============================ END OF BOUTON RECHERCHE ============================*/
