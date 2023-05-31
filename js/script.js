eee7121bfa71d9efd44bc04963f9c27e900cb621
LucasM548 <lucasm54800@gmail.com>
2023-05-31 19:23:27 +0200

    ggan

--- js/script.js
+++ js/script.js
@@ -236,9 +236,19 @@ const links = [
 /**======================
  *    GOOGLE ANALYTICS
  *========================**/
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://raw.githubusercontent.com/LucasM548/G-A/main/An-s.js','ga');
+
+ga('create', 'G-6MV2RQWCTV', 'auto');
+ga('send', 'pageview');
+
+/*
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'G-6MV2RQWCTV');
+*/
 /*==== END OF GOOGLE ANALYTICS ====*/
