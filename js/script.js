5d53f824071e054f10b319a3f6845d7b9ffdd52a
LucasM548 <lucasm54800@gmail.com>
2023-06-02 07:22:21 +0200

    mi analytics

--- js/script.js
+++ js/script.js
@@ -234,11 +234,17 @@ const links = [
 
 
 /**======================
- *    GOOGLE ANALYTICS
+ *    ANALYTICS
  *========================**/
 window.dataLayer = window.dataLayer || []
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'G-6MV2RQWCTV');
-/*==== END OF GOOGLE ANALYTICS ====*/
+
+(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "hdfqskztf2");
+/*==== END OF ANALYTICS ====*/
