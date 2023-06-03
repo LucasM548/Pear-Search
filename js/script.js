8724a1e15ae6eaef92f1d34d89a0689198dff968
LucasM548 <lucasm54800@gmail.com>
2023-06-04 01:14:17 +0200

    optimise js code

--- js/script.js
+++ js/script.js
@@ -1,66 +1,76 @@
 /**========================================================================
  *                           CHOIX LANGUE
  *========================================================================**/
+const translations = {
+  fr: {
+    translation: {
+      "Ap-π-cations": "Ap-π-cations",
+      "placeholder": "Écrivez pour trouver votre bonheur - Pear",
+      "search-choice": "Choix du navigateur",
+      "contribution": "Contribuer",
+    }
+  },
+  en: {
+    translation: {
+      "Ap-π-cations": "Ap-π-cations",
+      "placeholder": "Write to find your happiness - Pear",
+      "search-choice": "Choice of browser",
+      "contribution": "Contribution",
+    }
+  },
+  es: {
+    translation: {
+      "Ap-π-cations": "Aπ-licaciones",
+      "placeholder": "Escribe para encontrar tu felicidad - Pear",
+      "search-choice": "Elección del navegador",
+      "contribution": "Contribuir",
+    }
+  },
+  it: {
+    translation: {
+      "Ap-π-cations": "Aπ-plicazioni",
+      "placeholder": "Scrivi per trovare la tua felicità - Pear",
+      "search-choice": "Scelta del navigatore",
+      "contribution": "Contribuire",
+    }
+  },
+  ru: {
+    translation: {
+      "Ap-π-cations": "Приложе-π-ии",
+      "placeholder": "Пишите, чтобы найти свое счастье - Pear",
+      "search-choice": "Выбор браузера",
+      "contribution": "Сотрудничать",
+    }
+  },
+  zh: {
+    translation: {
+      "Ap-π-cations": "应用π",
+      "placeholder": "写信寻找你的幸福 - Pear",
+      "search-choice": "浏览器的选择",
+      "contribution": "贡献",
+    }
+  },
+  ko: {
+    translation: {
+      "Ap-π-cations": "응용프로-π-램",
+      "placeholder": "당신의 행복을 찾기 위해 글을 쓰세요 - Pear",
+      "search-choice": "브라우저 선택",
+      "contribution": "기여하다",
+    }
+  },
+  π: {
+    translation: {
+      "Ap-π-cations": "1 16 - π - 3 1 20 9 15 14 19",
+      "placeholder": "23 18 9 20 5   20 15   6 9 14 4   25 15 21 18   8 1 16 16 9 14 5 19 19 - Pear",
+      "search-choice": "3 8 15 9 3 5   15 6   2 18 15 23 19 5 18",
+      "contribution": "3 15 14 20 18 15 20 21 2 9 15 14",
+    }
+  },
+};
+
 i18next.init({
   lng: 'fr',
-  resources: {
-    fr: {
-      translation: {
-        "Ap-π-cations": "Ap-π-cations",
-        "placeholder": "Écrivez pour trouver votre bonheur - Pear",
-        "search-choice": "Choix du navigateur",
-        "contribution": "Contribuer",
-      }},
-    en: {
-      translation: {
-        "Ap-π-cations": "Ap-π-cations",
-        "placeholder": "Write to find your happiness - Pear",
-        "search-choice": "Choice of browser",
-        "contribution": "Contribution",
-      }},
-    es: {
-      translation: {
-        "Ap-π-cations": "Aπ-licaciones",
-        "placeholder": "Escribe para encontrar tu felicidad - Pear",
-        "search-choice": "Elección del navegador",
-        "contribution": "Contribuir",
-      }},
-    it: {
-      translation: {
-        "Ap-π-cations": "Aπ-plicazioni",
-        "placeholder": "Scrivi per trovare la tua felicità - Pear",
-        "search-choice": "Scelta del navigatore",
-        "contribution": "Contribuire",
-      }},
-    ru: {
-      translation: {
-        "Ap-π-cations": "Приложе-π-ии",
-        "placeholder": "Пишите, чтобы найти свое счастье - Pear",
-        "search-choice": "Выбор браузера",
-        "contribution": "Сотрудничать",
-      }},
-    zh: {
-      translation: {
-        "Ap-π-cations": "应用π",
-        "placeholder": "写信寻找你的幸福 - Pear",
-        "search-choice": "浏览器的选择",
-        "contribution": "贡献",
-      }},
-    ko: {
-      translation: {
-        "Ap-π-cations": "응용프로-π-램",
-        "placeholder": "당신의 행복을 찾기 위해 글을 쓰세요 - Pear",
-        "search-choice": "브라우저 선택",
-        "contribution": "기여하다",
-      }},
-    π: {
-      translation: {
-        "Ap-π-cations": "1 16 - π - 3 1 20 9 15 14 19",
-        "placeholder": "23 18 9 20 5   20 15   6 9 14 4   25 15 21 18   8 1 16 16 9 14 5 19 19 - Pear",
-        "search-choice": "3 8 15 9 3 5   15 6   2 18 15 23 19 5 18",
-        "contribution": "3 15 14 20 18 15 20 21 2 9 15 14",
-      }},
-  }
+  resources: translations,
 }, function(err, t) {
   updateContent();
 });
@@ -91,12 +101,10 @@ function changeLanguage(event) {
 window.addEventListener('load', function() {
   var language = localStorage.getItem('language');
   if (!language) {
-    // If no language is stored, use the user's preferred language
     var userLanguage = navigator.language.split('-')[0];
     if (['fr', 'en', 'es', 'it', 'ru', 'zh', 'ko', 'π'].includes(userLanguage)) {
       language = userLanguage;
     } else {
-      // If the user's preferred language is not supported, use English as the default language
       language = 'en';
     }
     localStorage.setItem('language', language);
@@ -118,23 +126,15 @@ window.addEventListener('load', function() {
  *=============================================**/
 const checkbox = document.getElementById('day-light');
 const root = document.documentElement;
+const theme = localStorage.getItem('theme');
 
-// Vérifie si le choix de l'utilisateur est déjà enregistré
-if (localStorage.getItem('theme') === 'Day-mode') {
-  root.classList.add('Day-mode');
-}
-
-// Met à jour l'apparence de la case à cocher en fonction du choix de l'utilisateur
-if (root.classList.contains('Day-mode')) {
-  checkbox.checked = false;
-} else {
-  checkbox.checked = true;
-}
+root.classList.toggle('Day-mode', theme === 'Day-mode');
+checkbox.checked = theme !== 'Day-mode';
 
 checkbox.addEventListener('change', function() {
   if (this.checked) {
     root.classList.remove('Day-mode');
-    localStorage.setItem('theme', '');
+    localStorage.removeItem('theme');
   } else {
     root.classList.add('Day-mode');
     localStorage.setItem('theme', 'Day-mode');
@@ -147,24 +147,25 @@ checkbox.addEventListener('change', function() {
  *                           APPS PANEL
  *========================================================================**/
 const appLinks = [
-    { href: "https://discord.com/", imgSrc: "img/Apps/π-Scord.png", alt: "π-Scord", text: "π-Scord" },
-    { href: "https://github.com/", imgSrc: "img/Apps/π-Hub.png", alt: "π-Hub", text: "π-Hub" },
-    { href: "https://www.paypal.com/", imgSrc: "img/Apps/π-Pal.png", alt: "π-Pal", text: "π-Pal" },
-    { href: "https://www.youtube.com/", imgSrc: "img/Apps/π-Tube.png", alt: "π-Tube", text: "π-Tube" },
-    { href: "https://translate.google.com/", imgSrc: "img/Apps/π-Translate.png", alt: "π-Translate", text: "π-Translate" },
-    { href: "https://www.google.com/maps/", imgSrc: "img/Apps/π-Maps.png", alt: "π-Maps", text: "π-Maps" },
-    { href: "https://play.google.com/", imgSrc: "img/Apps/π-Play.png", alt: "π-Play", text: "π-Play" },
-    { href: "https://docs.google.com/", imgSrc: "img/Apps/π-Docx.png", alt: "π-Docx", text: "π-Docx" },
-    { href: "https://sheets.google.com/", imgSrc: "img/Apps/π-Sheets.png", alt: "π-Sheets", text: "π-Sheets" },
-    { href: "https://slides.google.com/", imgSrc: "img/Apps/π-Slides.png", alt: "π-Slides", text: "π-Slides" },
-    { href: "https://forms.google.com/", imgSrc: "img/Apps/π-Forms.png", alt: "π-Forms", text: "π-Forms" },
-    { href: "https://earth.google.com/", imgSrc: "img/Apps/π-Earth.png", alt: "π-Earth", text: "π-Earth" },
-    { href: "https://meet.google.com/", imgSrc: "img/Apps/π-Meet.png", alt: "π-Meet", text: "π-Meet" },
-    { href: "https://news.google.com/", imgSrc: "img/Apps/π-News.png", alt: "π-News", text: "π-News" },
-    { href: "https://shopping.google.com/", imgSrc: "img/Apps/Shopp-π-G.png", alt: "Shopp-π-G", text: "Shopp-π-G" },
+  { href: "https://discord.com/", imgSrc: "img/Apps/π-Scord.png", alt: "π-Scord", text: "π-Scord" },
+  { href: "https://github.com/", imgSrc: "img/Apps/π-Hub.png", alt: "π-Hub", text: "π-Hub" },
+  { href: "https://www.paypal.com/", imgSrc: "img/Apps/π-Pal.png", alt: "π-Pal", text: "π-Pal" },
+  { href: "https://www.youtube.com/", imgSrc: "img/Apps/π-Tube.png", alt: "π-Tube", text: "π-Tube" },
+  { href: "https://translate.google.com/", imgSrc: "img/Apps/π-Translate.png", alt: "π-Translate", text: "π-Translate" },
+  { href: "https://www.google.com/maps/", imgSrc: "img/Apps/π-Maps.png", alt: "π-Maps", text: "π-Maps" },
+  { href: "https://play.google.com/", imgSrc: "img/Apps/π-Play.png", alt: "π-Play", text: "π-Play" },
+  { href: "https://docs.google.com/", imgSrc: "img/Apps/π-Docx.png", alt: "π-Docx", text: "π-Docx" },
+  { href: "https://sheets.google.com/", imgSrc: "img/Apps/π-Sheets.png", alt: "π-Sheets", text: "π-Sheets" },
+  { href: "https://slides.google.com/", imgSrc: "img/Apps/π-Slides.png", alt: "π-Slides", text: "π-Slides" },
+  { href: "https://forms.google.com/", imgSrc: "img/Apps/π-Forms.png", alt: "π-Forms", text: "π-Forms" },
+  { href: "https://earth.google.com/", imgSrc: "img/Apps/π-Earth.png", alt: "π-Earth", text: "π-Earth" },
+  { href: "https://meet.google.com/", imgSrc: "img/Apps/π-Meet.png", alt: "π-Meet", text: "π-Meet" },
+  { href: "https://news.google.com/", imgSrc: "img/Apps/π-News.png", alt: "π-News", text: "π-News" },
+  { href: "https://shopping.google.com/", imgSrc: "img/Apps/Shopp-π-G.png", alt: "Shopp-π-G", text: "Shopp-π-G" },
 ];
-  
+
 const appPanel = document.getElementById("appPanel");
+const applicationsLabel = document.querySelector('label[for="Applications"]');
 
 appLinks.forEach((app) => {
   const appElement = document.createElement("a");
@@ -186,35 +187,22 @@ appLinks.forEach((app) => {
 
 function toggleAppPanel(event) {
   event.stopPropagation();
-  const applicationsLabel = document.querySelector('label[for="Applications"]');
   appPanel.classList.toggle("visible");
   applicationsLabel.classList.toggle("active");
-  if (appPanel.classList.contains("visible")) {
-    applicationsLabel.classList.add("hovered");
-  } else {
-    applicationsLabel.classList.remove("hovered");
-  }
+  applicationsLabel.classList.toggle("hovered", appPanel.classList.contains("visible"));
 }
 
 function hideAppPanel() {
-  const applicationsLabel = document.querySelector('label[for="Applications"]');
   appPanel.classList.remove("visible");
-  applicationsLabel.classList.remove("active");
-  applicationsLabel.classList.remove("hovered");
+  applicationsLabel.classList.remove("active", "hovered");
 }
 
 document.addEventListener("click", function (event) {
-  const applicationsLabel = document.querySelector('label[for="Applications"]');
-  if (
-    !appPanel.contains(event.target) &&
-    event.target.id !== "Applications" &&
-    event.target.tagName !== "LABEL"
-  ) {
+  if (!appPanel.contains(event.target) && event.target.id !== "Applications" && event.target.tagName !== "LABEL") {
     hideAppPanel();
   }
 });
 
-const applicationsLabel = document.querySelector('label[for="Applications"]');
 applicationsLabel.addEventListener("mouseenter", function (event) {
   if (appPanel.classList.contains("visible")) {
     applicationsLabel.classList.add("hovered");
@@ -224,16 +212,6 @@ applicationsLabel.addEventListener("mouseenter", function (event) {
 applicationsLabel.addEventListener("mouseleave", function (event) {
   applicationsLabel.classList.remove("hovered");
 });
-
-document.addEventListener("click", function (event) {
-  if (
-    !appPanel.contains(event.target) &&
-    event.target.id !== "Applications" &&
-    event.target.tagName !== "LABEL"
-  ) {
-    hideAppPanel();
-  }
-});
 /*============================ END OF APPS PANEL ============================*/
 
 
@@ -241,33 +219,23 @@ document.addEventListener("click", function (event) {
  *               CHOIX MOTEUR
  *=============================================**/
 function getEngineURL(engine) {
-  switch (engine) {
-    case "Google":
-      return "https://www.google.com/search?q=";
-    case "Brave_search":
-      return "https://search.brave.com/search?q=";
-    case "DuckDuckGo":
-      return "https://duckduckgo.com/?q=";
-    default:
-      return "";
-  }
+  const engineURLs = {
+    Google: "https://www.google.com/search?q=",
+    Brave_search: "https://search.brave.com/search?q=",
+    DuckDuckGo: "https://duckduckgo.com/?q="
+  };
+
+  return engineURLs[engine] || "";
 }
 
 function getSelectedEngine() {
   const storedEngine = localStorage.getItem("selectedEngine");
-  if (storedEngine) {
-    return storedEngine;
-  } else {
-    return "Brave_search";
-  }
+  return storedEngine || "Brave_search";
 }
 
 function setSelectedEngine(engine) {
   localStorage.setItem("selectedEngine", engine);
-  $('input[name="search_engine"][value="' + engine + '"]').prop(
-    "checked",
-    true
-  );
+  $('input[name="search_engine"]').filter('[value="' + engine + '"]').prop("checked", true);
 }
 
 $(document).ready(function () {
@@ -288,18 +256,19 @@ $('input[name="search_engine"]').on("change", function () {
 function searchToggle(obj, evt) {
   const container = $(obj).closest(".search-wrapper");
   const selectedEngine = getSelectedEngine();
+  const searchInput = container.find(".search-input");
 
   if (!container.hasClass("active")) {
     container.addClass("active");
     evt.preventDefault();
   } else if (
-    container.find(".search-input").val().trim() !== "" &&
+    searchInput.val().trim() !== "" &&
     (evt.target.classList.contains("search-icon") ||
       evt.target.classList.contains("span") ||
       (evt.type === "keypress" &&
         (evt.key === "Enter" || evt.key === "Return")))
   ) {
-    const searchText = container.find(".search-input").val();
+    const searchText = searchInput.val();
     const webUrl = getEngineURL(selectedEngine);
     if (webUrl) {
       const finalURL = webUrl + encodeURIComponent(searchText);
@@ -310,7 +279,7 @@ function searchToggle(obj, evt) {
     !$(obj).closest(".input-holder").length
   ) {
     container.removeClass("active");
-    container.find(".search-input").val("");
+    searchInput.val("");
   }
 }
 
@@ -326,14 +295,14 @@ $(".search-input").on("keypress", function (evt) {
  *    BOUTON CONTRIBUTION
  *========================**/
 const links = [
-    "https://www.paypal.com/paypalme/LucasM54",
-    "https://www.paypal.com/paypalme/biquidouLIV"
-  ];
-  
-  function openRandomLink() {
-    const randomLink = links[Math.floor(Math.random() * links.length)];
-    window.open(randomLink, "_blank");
-  }  
+  "https://www.paypal.com/paypalme/LucasM54",
+  "https://www.paypal.com/paypalme/biquidouLIV"
+];
+
+function openRandomLink() {
+  const randomLink = links[Math.floor(Math.random() * links.length)];
+  window.open(randomLink, "_blank");
+}
 /*==== END OF BOUTON CONTRIBUTION ====*/
 
 
